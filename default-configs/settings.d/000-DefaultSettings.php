4b4ccb69002b58e53b3aa21dd41a8169439b752e
BenediktHofmann <hofmann@hallowelt.com>
2017-01-26 07:42:37 +0100

    Update 000-DefaultSettings.php
    Default settings for BlueSpice

--- default-configs/settings.d/000-DefaultSettings.php
+++ default-configs/settings.d/000-DefaultSettings.php
@@ -6,3 +6,21 @@ $wgDefaultUserOptions['timecorrection'] = 'ZoneInfo|' . (date("I") ? 120 : 60) .
 $wgUrlProtocols[] = 'file://';
 $wgNamespacesWithSubpages[NS_MAIN] = true;
 $wgExternalLinkTarget = '_blank';
+
+//Default MediaWiki settings needed for BlueSpice
+$GLOBALS['wgNamespacesWithSubpages'][NS_MAIN] = true;
+$GLOBALS['wgApiFrameOptions'] = 'SAMEORIGIN';
+$GLOBALS['wgRSSUrlWhitelist'] = array(
+	"http://blog.blue-spice.org/feed/",
+	"http://blog.bluespice.com/feed/",
+	"http://blog.hallowelt.com/feed/",
+);
+$GLOBALS['wgExternalLinkTarget'] = '_blank';
+$GLOBALS['wgCapitalLinkOverrides'][ NS_FILE ] = false;
+$GLOBALS['wgRestrictDisplayTitle'] = false; //Otherwise only titles that normalize to the same DB key are allowed
+$GLOBALS['wgUrlProtocols'][] = "file://";
+$GLOBALS['wgVerifyMimeType'] = false;
+$GLOBALS['wgAllowJavaUploads'] = true;
+
+//Skin specific
+$GLOBALS['wgDefaultSkin'] = 'bluespiceskin';
