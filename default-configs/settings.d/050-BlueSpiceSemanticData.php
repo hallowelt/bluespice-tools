cb78bc2c45f29d20f782c713327e33ca139ddc7a
Leonid Verhovskij <info@leonv.de>
2017-04-04 11:57:25 +0200

    Update 050-BlueSpiceSemanticData.php

--- default-configs/settings.d/050-BlueSpiceSemanticData.php
+++ default-configs/settings.d/050-BlueSpiceSemanticData.php
@@ -36,6 +36,6 @@ $GLOBALS[ 'sespUseAsFixedTables' ] = true;
 
 $GLOBALS[ 'wgSESPExcludeBots' ] = true;
 
-$GLOBALS["wgAutoloadClasses"]["SMW\Test\QueryPrinterRegistryTestCase"] = __DIR__."/extensions/SemanticMediaWiki/tests/phpunit/QueryPrinterRegistryTestCase.php";
-$GLOBALS["wgAutoloadClasses"]["SMW\Test\QueryPrinterTestCase"] = __DIR__."/extensions/SemanticMediaWiki/tests/phpunit/QueryPrinterTestCase.php";
+$GLOBALS["wgAutoloadClasses"]["SMW\Test\QueryPrinterRegistryTestCase"] = $IP . "/extensions/SemanticMediaWiki/tests/phpunit/QueryPrinterRegistryTestCase.php";
+$GLOBALS["wgAutoloadClasses"]["SMW\Test\QueryPrinterTestCase"] = $IP . "/extensions/SemanticMediaWiki/tests/phpunit/QueryPrinterTestCase.php";
 
