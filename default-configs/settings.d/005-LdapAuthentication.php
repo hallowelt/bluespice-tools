6f6e7b9c7739383ca399ba1d578623204da6b559
Benedikt Hofmann <hofmann@hallowelt.com>
2017-05-11 09:49:49 +0200

    Introduces array $bsgLDAPLocalUsers

--- default-configs/settings.d/005-LdapAuthentication.php
+++ default-configs/settings.d/005-LdapAuthentication.php
@@ -4,8 +4,12 @@ require_once "$IP/extensions/LdapAuthentication/LdapAuthentication.php";
 require_once "$IP/extensions/LdapAuthentication/LdapAutoAuthentication.php";
 require_once "$IP/extensions/BlueSpiceLdapAuthenticationConnector/LdapAuthenticationConnector.setup.php";
 
-if ( ( !( isset($_POST['wpName']) && $_POST['wpName'] === 'Wikisysop' ) ) &&
-     PHP_SAPI != 'cli' &&
-     file_exists( "$IP/LocalSettings.ldap.php" ) ) {
-        require_once "$IP/LocalSettings.ldap.php";
+if ( !isset( $bsgLDAPLocalUsers ) ) {
+	$bsgLDAPLocalUsers[] = 'Wikisysop';
+}
+
+if ( file_exists( "$IP/LocalSettings.ldap.php" ) && PHP_SAPI != 'cli' ) {
+	if ( isset( $_POST['wpName'] ) && in_array( $_POST['wpName'], $bsgLDAPLocalUsers ) ) {
+        require_once "$IP/LocalSettings.ldap.php";
+	}
 }
