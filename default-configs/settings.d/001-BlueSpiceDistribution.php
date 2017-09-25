38f9d70bdf911ca9666c6ff1f97f7cec3a638c1d
Leonid Verhovskij <info@leonv.de>
2017-09-25 15:31:07 +0200

    revert rss inclusion for backward compatability

--- default-configs/settings.d/001-BlueSpiceDistribution.php
+++ default-configs/settings.d/001-BlueSpiceDistribution.php
@@ -6,7 +6,7 @@ require_once "$IP/extensions/HitCounters/HitCounters.php";
 require_once "$IP/extensions/ImageMapEdit/ImageMapEdit.php";
 require_once "$IP/extensions/Lockdown/Lockdown.php";
 require_once "$IP/extensions/Quiz/Quiz.php";
-wfLoadExtension('RSS');
+require_once "$IP/extensions/RSS/RSS.php";
 require_once "$IP/extensions/Echo/Echo.php";
 require_once "$IP/extensions/TitleKey/TitleKey.php";
 require_once "$IP/extensions/EmbedVideo/EmbedVideo.php";
