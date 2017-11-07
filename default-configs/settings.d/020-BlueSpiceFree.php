1af7feb1d2ceec194bde2655eb1e89bd3ff487a4
Benedikt Hofmann <hofmann@hallowelt.com>
2017-11-07 13:07:14 +0100

    Update 020-BlueSpiceFree.php

--- default-configs/settings.d/020-BlueSpiceFree.php
+++ default-configs/settings.d/020-BlueSpiceFree.php
@@ -2,5 +2,4 @@
 
 require_once "$IP/extensions/BlueSpiceFoundation/BlueSpiceFoundation.php";
 require_once "$IP/extensions/BlueSpiceExtensions/BlueSpiceExtensions.php";
-require_once "$IP/extensions/BlueSpiceTagCloud/BlueSpiceTagCloud.php";
 require_once "$IP/skins/BlueSpiceSkin/BlueSpiceSkin.php";
