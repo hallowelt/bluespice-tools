4db298987bbb0720de8e310652c694f7e4f05c85
Benedikt Hofmann <hofmann@hallowelt.com>
2018-01-23 09:26:11 +0100

    Update LocalSettings.BlueSpice.php

--- default-configs/LocalSettings.BlueSpice.php
+++ default-configs/LocalSettings.BlueSpice.php
@@ -8,8 +8,8 @@ if ( !defined( 'MEDIAWIKI' ) ) {
     exit;
 }
 
-if ( file_exists( __DIR__ . "/LocalSettings.local.php" ) ) {
-	require_once __DIR__ . "/LocalSettings.local.php";
+if ( file_exists( "$IP/LocalSettings.local.php" ) ) {
+	require_once "$IP/LocalSettings.local.php";
 }
 
 if ( isset( $bsgSettingsDir ) ) {
