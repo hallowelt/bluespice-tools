76c2b9b45287d8776a5fdf65eede1f6d23f0e7a2
Benedikt Hofmann <hofmann@hallowelt.com>
2018-04-25 16:44:12 +0200

    Update 000-ArticlePath.php

--- default-configs/settings.d/000-ArticlePath.php
+++ default-configs/settings.d/000-ArticlePath.php
@@ -10,7 +10,23 @@
  * RewriteEngine On
  * RewriteRule ^/?wiki(/.*)?$ %{DOCUMENT_ROOT}/index.php [L]
  *
+ * For IIS integration please add the following rewrite ruke to you web.config:
+ *
+ * <?xml version="1.0" encoding="UTF-8"?>
+ * <configuration>
+ *     <system.webServer>
+ *         <rewrite>
+ *             <rules>
+ *                 <rule name="WikiShortURL">
+ *                     <match url="^wiki/(.*)$" />
+ *                     <action type="Rewrite" url="/index.php?title={UrlEncode:{R:1}}" />
+ *                 </rule>
+ *             </rules>
+ *         </rewrite>
+ *     </system.webServer>
+ * </configuration>
+ *
  */
- 
- $wgArticlePath = "/wiki/$1";
+
+$wgArticlePath = "/wiki/$1";
  
