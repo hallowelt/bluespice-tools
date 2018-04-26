5921f1356ad87d572b5765b7777152763f17b7c3
Benedikt Hofmann <hofmann@hallowelt.com>
2018-04-26 11:38:59 +0200

    Update 050-BlueSpiceTeamwork.php

--- default-configs/settings.d/050-BlueSpiceTeamwork.php
+++ default-configs/settings.d/050-BlueSpiceTeamwork.php
@@ -4,3 +4,5 @@ require_once "$IP/extensions/BlueSpiceTeamwork/BlueSpiceTeamwork.php";
 
 $wgDefaultUserOptions['echo-subscriptions-email-bs-review-action-cat'] = 1;
 $wgDefaultUserOptions['echo-subscriptions-email-bs-review-assignment-cat'] = 1;
+
+$wgFlaggedRevsHandleIncludes = FR_INCLUDES_FREEZE;
