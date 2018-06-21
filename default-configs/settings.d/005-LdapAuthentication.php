7cd7cf5875679b9b26ec452859554ed284a1455d
Benedikt Hofmann <hofmann@hallowelt.com>
2018-06-21 09:09:51 +0200

    Update 005-LdapAuthentication.php
    Removed unneeded blank lines; changed position of $wgDisableAuthManager because it caused problems at farming.

--- default-configs/settings.d/005-LdapAuthentication.php
+++ default-configs/settings.d/005-LdapAuthentication.php
@@ -4,6 +4,8 @@ require_once "$IP/extensions/LdapAuthentication/LdapAuthentication.php";
 require_once "$IP/extensions/LdapAuthentication/LdapAutoAuthentication.php";
 require_once "$IP/extensions/BlueSpiceLdapAuthenticationConnector/LdapAuthenticationConnector.setup.php";
 
+$wgDisableAuthManager = true;
+
 if ( !isset( $bsgLDAPLocalUsers ) ) {
 	$bsgLDAPLocalUsers[] = 'Wikisysop';
 }
@@ -11,16 +13,12 @@ if ( !isset( $bsgLDAPLocalUsers ) ) {
 if ( isset( $_POST['wpName'] ) && in_array( $_POST['wpName'], $bsgLDAPLocalUsers ) ) {
 	return;
 }
-
 elseif ( PHP_SAPI == 'cli' ) {
 	return;
 }
-
 elseif ( !file_exists( "$IP/LocalSettings.ldap.php" ) ) {
 	return;
 }
-
 else {
-	$wgDisableAuthManager = true;
 	require_once "$IP/LocalSettings.ldap.php";
 }
