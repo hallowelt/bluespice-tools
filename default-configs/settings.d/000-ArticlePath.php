14627b8470cc8d516bc093f81c0483a6ad98be61
Benedikt Hofmann <hofmann@hallowelt.com>
2019-09-10 09:47:41 +0200

    Update 000-ArticlePath.php

--- default-configs/settings.d/000-ArticlePath.php
+++ default-configs/settings.d/000-ArticlePath.php
@@ -12,18 +12,26 @@
  *
  * For IIS integration please add the following rewrite ruke to you web.config:
  *
- * <?xml version="1.0" encoding="UTF-8"?>
+ * <?xml version="1.0" encoding="utf-8"?>
  * <configuration>
- *     <system.webServer>
- *         <rewrite>
- *             <rules>
- *                 <rule name="WikiShortURL">
- *                     <match url="^wiki/(.*)$" />
- *                     <action type="Rewrite" url="/w/index.php?title={UrlEncode:{R:1}}" />
- *                 </rule>
- *             </rules>
- *         </rewrite>
- *     </system.webServer>
+ *   <system.webServer>
+ *     <rewrite>
+ *       <rules>
+ *         <rule name="Wiki-Short-URL-1" stopProcessing="true">
+ *           <match url="^wiki/(.*)$" />
+ *           <action type="Rewrite" url="/w/index.php?title={UrlEncode:{R:1}}" />
+ *         </rule>
+ *         <rule name="Wiki-Short-URL-2" stopProcessing="true">
+ *           <match url="^wiki/$" />
+ *           <action type="Rewrite" url="/w/index.php" />
+ *         </rule>
+ *         <rule name="Wiki-Short-URL-3" stopProcessing="true">
+ *           <match url="^/*$" />
+ *           <action type="Rewrite" url="/w/index.php" />
+ *         </rule>
+ *       </rules>      
+ *     </rewrite>
+ *   </system.webServer>
  * </configuration>
  *
  */
