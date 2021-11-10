abb2a1717562140bb4d4e2befe41ff56c46c33a1
Benedikt Hofmann <hofmann@hallowelt.com>
2021-11-10 07:59:21 +0100

    Changed old syntax of inkscape to new one

--- default-configs/settings.d/005-Inkscape.php
+++ default-configs/settings.d/005-Inkscape.php
@@ -1,6 +1,9 @@
 <?php
 
-$wgSVGConverters = array( 'inkscape' => '"/usr/bin/inkscape" -z -w $width -f $input -e $output' ); 
+# Old "version". Changed with Inkscape >= 1.x
+#$wgSVGConverters = array( 'inkscape' => '"/usr/bin/inkscape" -z -w $width -f $input -e $output' );
+
+$wgSVGConverters = [ 'inkscape' => '"E:\bluespice\bin\inkscape-1.1\bin\inkscape.exe" --export-filename $output -w $width $input' ]; 
 $wgSVGConverter = 'inkscape';
 $wgMaxShellMemory = 4096000;
 $wgSVGMaxSize = 2048;
