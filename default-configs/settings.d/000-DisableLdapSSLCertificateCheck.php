21292a33d688220c3e88ca73bd7abc1091f02422
Benedikt Hofmann <hofmann@hallowelt.com>
2022-03-18 08:48:08 +0100

    Update 000-DisableLdapSSLCertificateCheck.php

--- default-configs/settings.d/000-DisableLdapSSLCertificateCheck.php
+++ default-configs/settings.d/000-DisableLdapSSLCertificateCheck.php
@@ -3,7 +3,8 @@
 /*
  *
  * Disables check of certificate for LDAP connections.
- * To ensure that it works without problems create /etc/openldap/ldap.conf (Windows: C:\OpenLDAP\ldap.conf)
+ * To ensure that it works without problems create /etc/openldap/ldap.conf
+ * (Windows: C:\OpenLDAP\ldap.conf OR C:\OpenLDAP\sysconf\ldap.conf - yust try it)
  * Content: "TLS_REQCERT never"
  *
  */
